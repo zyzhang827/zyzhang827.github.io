5425ab29b3f708c00ce0dd8e18e5e960f3979ca7
zyzhang827 <1194749361@qq.com>
2022-01-03 19:22:21 +0800

    Site updated: 2022-01-03 19:22:19

--- js/show_text.js
+++ js/show_text.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
         var x = e.pageX,
         y = e.pageY;
         $i.css({
-            "z-index": 5,
+            "z-index": 6,
             "top": y - 20,
             "left": x,
             "position": "absolute",
